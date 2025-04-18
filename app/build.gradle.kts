0327cfcadb6c07a935c6ef7b30c92d8e7114787f
burakselcuk <design.burak1@gmail.com>
2025-04-18 11:25:45 +0300

    Updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,7 +110,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "com.github.burakselcuk1"
                 artifactId = "MySdk"
-                version = "v1.0.6"
+                version = "v1.0.7"
 
                 from(components["release"])
 
--- app/src/main/java/com/burak/mysdk/mySdkApp.kt
+++ app/src/main/java/com/burak/mysdk/mySdkApp.kt
@@ -3,7 +3,17 @@ package com.burak.mysdk
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-@HiltAndroidApp
-class mySdkApp: Application() {
+class mySdkApp : Application() {
+    companion object {
+        private var instance: mySdkApp? = null
 
+        fun getInstance(): mySdkApp {
+            return instance!!
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
 }
